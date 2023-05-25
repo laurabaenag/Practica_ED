a368250ba9370a20af76e201bced1994cdcb99c6
Laura <lbaenagarcia@iesnicolascopernico.com>
2023-05-25 13:57:41 +0200

    Añadiendo los java Docs en el primer metodo NumeroAleatorio.java.

--- src/practicaED/bloque1/NumeroAleatorio.java
+++ src/practicaED/bloque1/NumeroAleatorio.java
@@ -1,9 +1,19 @@
 package practicaED.bloque1;
 
+/**
+ * @author Laura
+ * Implementa el primer metodo de numeroAleatorio.
+ *
+ */
 public class NumeroAleatorio {
-
+	/*Método que devuelva un número aleatorio entre dos pasados por
+	argumentos del método.*/
+	/*
+	 * @param numero1 y numero2
+	 * Le introduczo al metodo los parametros anteriores*/
 	public int numeroAleatorio(int numero1, int numero2) {
 		int numeroRandom = (int) (Math.random() * (numero1 - numero2 + 1) + numero2);
+		/*Me devuelve el numero random*/
 		return numeroRandom;
 	}
 
